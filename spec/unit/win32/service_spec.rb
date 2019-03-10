dd4b0d61583d25a0137d06c946042f8072dfe928
Jason Barnett <jason.w.barnett@gmail.com>
2019-03-10 16:52:41 -0700

    add

--- spec/unit/win32/service_spec.rb
+++ spec/unit/win32/service_spec.rb
@@ -4,11 +4,16 @@ describe Win32::Service do
   describe '::open_sc_manager' do
     context 'when passed a block' do
       it 'yields a pointer to a scm handle' do
+        a = nil
+        p a
         described_class.open_sc_manager do |scm_handle|
+          a = scm_handle
+          p a
           expect(scm_handle).not_to be_nil
           expect(scm_handle).not_to eq(0)
           expect(scm_handle).to be_a(Integer)
         end
+        p a
       end
     end
   end
