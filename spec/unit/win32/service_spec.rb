a7de14789e5651eac572aeb20a8cca2edf90610d
Jason Barnett <jason.w.barnett@gmail.com>
2019-03-10 16:47:15 -0700

    Add another test

--- spec/unit/win32/service_spec.rb
+++ spec/unit/win32/service_spec.rb
@@ -6,6 +6,7 @@ describe Win32::Service do
       described_class.open_sc_manager do |scm_handle|
         expect(scm_handle).not_to be_nil
         expect(scm_handle).not_to eq(0)
+        expect(scm_handle).to be_a(Integer)
       end
     end
   end
