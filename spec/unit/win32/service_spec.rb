56777b85735ebfb75ba2d0cec10ffdb2bd37728f
Jason Barnett <jason.w.barnett@gmail.com>
2019-03-10 16:34:45 -0700

    update unit tests

--- spec/unit/win32/service_spec.rb
+++ spec/unit/win32/service_spec.rb
@@ -3,8 +3,10 @@ require 'win32/service'
 describe Win32::Service do
   describe '::open_sc_manager' do
     it 'returns a pointer' do
-      expect(described_class.open_sc_manager).not_to be_nil
-      expect(described_class.open_sc_manager).not_to be_null
+      open_sc_manager do |scm_handle|
+        expect(scm_handle).not_to be_nil
+        expect(scm_handle).not_to eq(0)
+      end
     end
   end
 end
